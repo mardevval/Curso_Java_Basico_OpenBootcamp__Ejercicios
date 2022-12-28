c2960b8bb605cddf0bd4a7b422414a6afdfaa68e
Mar Dev Val <mardevval.gmail.com>
2022-12-28 16:05:43 +0100

    commit-001

---
+++ src/com/openBootCamp/ejercicios_parte2/TipoDatos.java
@@ -0,0 +1,53 @@
+package com.openBootCamp.ejercicios_parte2;
+
+public class TipoDatos {
+
+    public static void main(String[] args) {
+
+
+        // INTEGERS
+
+        byte variable1 = 10;
+        System.out.println(variable1);
+
+        short variable2 = 100;
+        System.out.println(variable2);
+
+        int variable3 = 1000;
+        System.out.println(variable3);
+
+        long variable4 = 1000000000;
+        System.out.println(variable4);
+
+
+
+        // DECIMALS
+
+        float variable5 = 37.7f;
+        System.out.println(variable5);
+
+        double variable6 = 723843.34d;
+        System.out.println(variable6);
+
+
+
+        // BOOLEANS
+
+        boolean variable7 = true;
+        System.out.println(variable7);
+
+        boolean variable8 = false;
+        System.out.println(variable8);
+
+
+
+        // CHARACTERS
+
+        char variable9 = 'g';
+        System.out.println(variable9);
+
+        String variable10 = "En un lugar de la mancha, de cuyo nombre no quiero ...";
+        System.out.println(variable10);
+    }
+
+}
